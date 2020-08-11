1521c182779ae562111ea16c0b1469bfd4cedc84
Shadman <shadmanchowdhury87@gmail.com>
2020-08-11 21:38:30 +0600

    Procfile and requirements.txt added

--- todo_rest_api/todo_rest_api/settings.py
+++ todo_rest_api/todo_rest_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@^-+j7qeo^pwmvp87(y1zb3!pnk3*uwm26)fjk3q6p-24kqsl1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['interactive-todo-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
